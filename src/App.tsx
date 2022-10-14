644ce9d01fcaa877a8ae14b41f89cc2d0b9aa71a
Zachary Lee <zacharylhlee@gmail.com>
2022-10-14 16:40:01 -0400

    Added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import React from 'react';
-import NavigationBar from './components/NavigationBar/NavigationBar';
+import React from "react";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Home from "./pages/Home/Home";
 
 const App = () => (
-  <NavigationBar></NavigationBar>
-  
+  <Router>
+    <NavigationBar />
+    <Routes>
+      <Route path="home" element={<Home />} />
+    </Routes>
+  </Router>
 );
 
 export default App;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import "./Home.css";
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+const Home = () => (
+  <Container>
+    <h1>Zachary Lee</h1>
+  </Container>
+);
+
+export default Home;
