117e41406b0203772b139ce625ae00b6d5685bc4
Zachary Lee <zacharylhlee@gmail.com>
2022-10-14 16:31:06 -0400

    Created NavigationBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import React from 'react';
+import NavigationBar from './components/NavigationBar/NavigationBar';
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
+  <NavigationBar></NavigationBar>
+  
 );
 
 export default App;
---
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -0,0 +1,25 @@
+import "./NavigationBar.css";
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const NavigationBar = () => (
+  <Navbar bg="light" expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">Zachary Lee</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#about-me">About Me</Nav.Link>
+          <Nav.Link href="#education">Eductation</Nav.Link>
+          <Nav.Link href="#lighthouse-labs">Lighthouse Labs</Nav.Link>
+          <Nav.Link href="#employment">Employment</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+);
+
+export default NavigationBar;
