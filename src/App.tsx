d9a56ddbd66e9dc2286ea6b19492ab079bfc1f02
Zachary Lee <zacharylhlee@gmail.com>
2022-10-14 16:59:01 -0400

    Added Navigate to Home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
 import React from "react";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
@@ -10,6 +15,7 @@ const App = () => (
   <Router>
     <NavigationBar />
     <Routes>
+      <Route path="/" element={<Navigate replace to="/home" />} />
       <Route path="home" element={<Home />} />
     </Routes>
   </Router>
