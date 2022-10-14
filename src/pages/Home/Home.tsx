5a021e1d8241b1e1b76b5adff14816c9cdee5d82
Zachary Lee <zacharylhlee@gmail.com>
2022-10-14 16:51:04 -0400

    Created Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,30 @@
 import "./Home.css";
 import React from "react";
-import Container from "react-bootstrap/Container";
+import { Container } from "react-bootstrap";
 
 const Home = () => (
-  <Container>
+  <Container className="home-container">
     <h1>Zachary Lee</h1>
+    <span>Full-Stack Developer</span>
+    <br />
+    <span>
+      email: <a href="mailto:zacharylhlee@gmail.com">zacharylhlee@gmail.com</a>
+    </span>
+    <br />
+    <span>phone: +1 (438) 992-9667</span>
+    <br />
+    <span>
+      LinkedIn:{" "}
+      <a href="https://www.linkedin.com/in/zachary-lee-850b2314a">
+        linkedin.com/in/zachary-lee-850b2314a
+      </a>
+    </span>
+    <br />
+    <span>
+      GitHub:{" "}
+      <a href="https://www.github.com/zacharylee97">github.com/zacharylee97</a>
+    </span>
+    <br />
   </Container>
 );
 
