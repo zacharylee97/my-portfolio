4074d1bfd0afb4e660229bfaf1ceb92b3d972778
Zachary Lee <zacharylhlee@gmail.com>
2022-10-15 10:06:55 -0400

    Fixed test function

--- functions/test.tsx
+++ functions/test.tsx
@@ -4,7 +4,9 @@ exports.handler = async (event, context) => {
   try {
     const limit = 12;
     const token = process.env.REACT_APP_INS_TOKEN;
-    const response = await fetch(`https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${limit}&access_token=${token}`;
+    const response = await fetch(
+      `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${limit}&access_token=${token}`
+    );
     const data = await response.json();
     return { statusCode: 200, body: JSON.stringify({ data }) };
   } catch (error) {
