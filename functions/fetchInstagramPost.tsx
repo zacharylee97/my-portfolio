7d1c1330e5f98e3800a8fa7e384dfc33f1c70d44
Zachary Lee <zacharylhlee@gmail.com>
2022-10-15 09:36:45 -0400

    Fixed fetchInstagramPost

--- functions/fetchInstagramPost.tsx
+++ functions/fetchInstagramPost.tsx
@@ -1,8 +1,17 @@
 import axios from "axios";
 
-export const fetchInstagramPost = async (limit: number) => {
+exports.handler = async () => {
+  const limit = 12;
   const token = process.env.REACT_APP_INS_TOKEN;
-  return axios.get(
-    `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${limit}&access_token=${token}`
-  );
+  try {
+    return axios.get(
+      `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${limit}&access_token=${token}`
+    );
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed fetching data" }),
+    };
+  }
 };
