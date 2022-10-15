577ada8ddd8955e3716b6deccbb0b5d8fa3b9571
Zachary Lee <zacharylhlee@gmail.com>
2022-10-15 09:42:05 -0400

    Added console.log

--- functions/fetchInstagramPost.tsx
+++ functions/fetchInstagramPost.tsx
@@ -4,6 +4,7 @@ exports.handler = async () => {
   const limit = 12;
   const token = process.env.REACT_APP_INS_TOKEN;
   try {
+    console.log(token);
     return axios.get(
       `https://graph.instagram.com/me/media?fields=id,media_type,media_url,caption&limit=${limit}&access_token=${token}`
     );
