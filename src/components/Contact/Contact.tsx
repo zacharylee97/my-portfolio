30dc9979fe5683de30cb755ce70115ca4964b96c
Zachary Lee <zacharylhlee@gmail.com>
2023-11-16 07:29:15 +0800

    Added profile pic

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,38 +1,45 @@
 import "./Contact.css";
-import { Container } from "react-bootstrap";
+import { Col, Container, Image } from "react-bootstrap";
 
 const Contact = () => (
-  <Container className="page-container">
-    <h1>Zachary Lee</h1>
-    <span>Full Stack Software Developer</span>
-    <br />
-    <span>
-      Email: <a href="mailto:zacharylhlee@gmail.com">zacharylhlee@gmail.com</a>
-    </span>
-    <br />
-    <span>Phone: +1 (438) 992-9667</span>
-    <br />
-    <span>
-      Instagram:{" "}
-      <a href="https://www.instagram.com/zachary.lee97">
-        https://www.instagram.com/zachary.lee97
-      </a>
-    </span>
-    <br />
-    <span>
-      LinkedIn:{" "}
-      <a href="https://www.linkedin.com/in/zachary-lee-850b2314a">
-        https://www.linkedin.com/in/zachary-lee-850b2314a
-      </a>
-    </span>
-    <br />
-    <span>
-      GitHub:{" "}
-      <a href="https://www.github.com/zacharylee97">
-        https://www.github.com/zacharylee97
-      </a>
-    </span>
-    <br />
+  <Container className="contact-container">
+    <Container>
+      <h1>Zachary Lee</h1>
+      <span>Full Stack Software Developer</span>
+      <br />
+      <span>
+        Email:{" "}
+        <a href="mailto:zacharylhlee@gmail.com">zacharylhlee@gmail.com</a>
+      </span>
+      <br />
+      <span>Phone: +1 (438) 992-9667</span>
+      <br />
+      <span>
+        Instagram:{" "}
+        <a href="https://www.instagram.com/zachary.lee97">
+          https://www.instagram.com/zachary.lee97
+        </a>
+      </span>
+      <br />
+      <span>
+        LinkedIn:{" "}
+        <a href="https://www.linkedin.com/in/zachary-lee-850b2314a">
+          https://www.linkedin.com/in/zachary-lee-850b2314a
+        </a>
+      </span>
+      <br />
+      <span>
+        GitHub:{" "}
+        <a href="https://www.github.com/zacharylee97">
+          https://www.github.com/zacharylee97
+        </a>
+      </span>
+    </Container>
+    <Container>
+      <Col xs={6} md={4}>
+        <Image src="/profile-picture.jpg" thumbnail />
+      </Col>
+    </Container>
   </Container>
 );
 
