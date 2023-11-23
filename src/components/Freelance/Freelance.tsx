4cf150c6c575d78c1b82c929a2009ed67baff0c5
Zach Lee <zacharylhlee@gmail.com>
2023-11-23 08:15:58 -0700

    Updated Freelance

--- src/components/Freelance/Freelance.tsx
+++ src/components/Freelance/Freelance.tsx
@@ -4,12 +4,22 @@ const Freelance = () => (
   <div className="freelance-container">
     <h1>Freelance</h1>
     <div className="project">
+      <b>Queer Aquarian</b>
+      <br />
+      <a href="https://queer-aquarian.netlify.app">
+        https://queer-aquarian.netlify.app
+      </a>
+      <p>November 2023 to Current</p>
+      <p>Website for Queer Aquarian Inc.</p>
+      <p>TECH: Next.js, Typescript, Tailwind CSS</p>
+    </div>
+    <div className="project">
       <b>FLC Consulting</b>
       <br />
       <a href="https://flc-consulting.netlify.app">
         https://flc-consulting.netlify.app
       </a>
-      <p>August 2023 to current</p>
+      <p>August 2023 to October 2023</p>
       <p>Website for FLC Consulting</p>
       <p>TECH: Next.js, Typescript, Tailwind CSS</p>
     </div>
