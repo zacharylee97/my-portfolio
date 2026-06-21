672d5452d90a55ae14ea6e20b161f5b49052c21c
Zach Lee <zacharylhlee@gmail.com>
2026-06-21 16:13:06 -0600

    Updated About Me

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -4,16 +4,14 @@ const AboutMe = () => (
   <div className="about-me-container">
     <h1>About Me</h1>
     <p>
-      I am a full-stack web developer with a background in life sciences. Having
-      worked in multiple research labs, I have experience collaborating on a
-      project and I have enjoyed working in a group setting. I decided to become
-      a web developer because I became interested in computer science after I
-      took a basic Java course in university. I am an artistic and creative
-      person, which lends itself to front-end development and user interface
-      design. I also like the challenge of thinking logically when working on
-      back-end development. Through my work experience, I’ve found that I enjoy
-      working on the database side of things as well, designing the structure
-      and writing queries.
+      My name is Zachary and I am a graduate of McGill University with a
+      Bachelor of Science majoring in Physiology. After I graduated, I completed
+      a four month full-time Web Development bootcamp at Lighthouse Labs and I
+      was hired at Beslogic Inc. as a Full Stack Software Developer. I am
+      currently working as an Academic Teacher and Developmental Support
+      Assistant at Link and Learn Therapy Clinic & Academy. I am planning to
+      going back to university to study Psychology and work towards becoming a
+      therapist.
     </p>
   </div>
 );
